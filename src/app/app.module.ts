4473cf1fd7828a06bf704d7bd61d8aeec90fe394
jmlinares2019 <jlinaresmar@gmail.com>
2020-09-20 20:20:15 +0200

    First code commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material';
+// import { MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material'; no longer can be imported from @angular/material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
-@Component{(
+@Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
-)}
+})
 export class LoginComponent {
 
 }
