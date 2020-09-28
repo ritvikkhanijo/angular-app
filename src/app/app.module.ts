8f1980ff1f686ec3ca2f9f71243c4da32fbeaebf
jmlinares2019 <jlinaresmar@gmail.com>
2020-09-28 17:03:11 +0200

    Deletes RegisterComponent.ts (don't know how created)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-// import { MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material'; no longer can be imported from @angular/material
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { FormsModule } from '@angular/forms';
-import { RegisterComponent } from "./register/RegisterComponent";
-import { RouterModule } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
 import { NavbarComponent } from './navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 import { ManageReservationsComponent } from './manage-reservations/manage-reservations.component';
 import { CreateCarComponent } from './create-car/create-car.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
 import { AdminUsersComponent } from './admin-users/admin-users.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material'; no longer can be imported from @angular/material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
-
 
 
 @NgModule({
