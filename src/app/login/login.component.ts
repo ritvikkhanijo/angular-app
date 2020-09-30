6309c55fb2fed3e273a7d886444b363ec3a480d4
jmlinares2019 <jlinaresmar@gmail.com>
2020-09-30 20:45:01 +0200

    Creates Frontend login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -7,11 +9,40 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+    token: any;
+
+    constructor(private userservice: UserService, private router: Router){}
+
     onlogin(form: NgForm){
         const email = form.value.email;
         const password = form.value.password;
-        this.userservice.loginUser(email, password).subscribe(result => {
+        this.userservice.loginUser(email,password).subscribe(result =>{
+            this.userservice.authenticated.next(true);
+            const admin = result.admin;
+            const token = result.token;
+            this.userservice.isAdmin.next(admin);
+            const expires = result.expiresIn;
+            if(token){
+                const now = new Date();
+                const expirationDate = new Date(now.getTime() + expires*1000);
+                this.userservice.saveuserData(token, expirationDate, admin);
+                this.router.navigate(['/main']);
+            }
+        });
     
-        })
+    };
+
+    autoAuthUser(){
+        const authinfo = this.userservice.getUserData();
+        if (!authinfo){
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authinfo.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0){
+            this.token = authinfo.token;
+            this.userservice.authenticated.next(true);
+        }
     }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class UserService {
 
-  constructor(private http: HttpClient) {}
+  tokenTimer: any;
+  isAdmin = new Subject();
+  authenticated = new BehaviorSubject(false);
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   createUser(email: string, password: string){
     const authData = {
@@ -26,5 +32,43 @@ export class UserService {
       admin: any
     }>
     ('http://localhost:3000/api/user/signup', authData);
+  }
+
+  changeAdmin(data: any) {
+    this.isAdmin.next(data);
+  }
+
+  setTimer(duration: any){
+    this.tokenTimer = setTimeout(() => {
+      this.onLogout()
+    }, duration*1000);
+  }
+
+  onLogout(){
+    this.authenticated.next(false);
+    clearTimeout(this.tokenTimer);
+    this.changeAdmin(0);
+    localStorage.removeAll();
+    this.router.navigate(['']);
+  }
+
+  getUserData(){
+    const token = localStorage.getItem('token');
+    const expiration = localStorage.getItem('expiration');
+    const admin = localStorage.getItem('admin');
+    if (!token || ! expiration){
+      return;
+    }
+    return{
+      token: token,
+      expirationDate: new Date(expiration),
+      admin: admin
+    }
+  }
+
+  saveuserData(token: string, expiration: Date, admin: any){
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expiration.toString());
+    localStorage.setItem('admin', admin);
   }
 }
